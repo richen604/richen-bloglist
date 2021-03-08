74ba9bb8c2f3b5c338056add2179b8a980900f4b
richen604 <56615615+richen604@users.noreply.github.com>
2021-03-08 15:47:30 -0800

    added return to uncaught exceptions for cypress (testing)

--- cypress/integration/blog_app.spec.js
+++ cypress/integration/blog_app.spec.js
@@ -52,19 +52,10 @@ const newUser = {
   password: 'test123',
 }
 
-/* cy.on('uncaught:exception', (err, runnable) => {
+Cypress.on('uncaught:exception', (err, runnable) => {
   // returning false here prevents Cypress from
   // failing the test
   return false
-}) */
-
-cy.on('fail', (error, runnable) => {
-  debugger
-
-  // we now have access to the err instance
-  // and the mocha runnable this failed on
-
-  throw error // throw error to have test still fail
 })
 
 describe('Blog app', function () {
