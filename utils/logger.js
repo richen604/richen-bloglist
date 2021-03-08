ba93c0c46967d9648962b853bfc5971a6a2e2c8d
richen604 <56615615+richen604@users.noreply.github.com>
2021-03-08 14:16:27 -0800

    github actions fixes

--- utils/logger.js
+++ utils/logger.js
@@ -1,10 +1,8 @@
 const info = (...params) => {
-  if (process.env.NODE_ENV !== 'test') {
+  if (process.env.NODE_ENV === 'test') {
     console.log(...params)
     return
   }
-  console.log(...params)
-  return
 }
 
 const error = (...params) => {
