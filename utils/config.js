57e7f7c845d56616e227a34b15ef8a3e36aab291
richen604 <56615615+richen604@users.noreply.github.com>
2021-03-23 20:58:51 -0700

    changes to cypress testing, currently breaking

--- utils/config.js
+++ utils/config.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const PORT = process.env.PORT
 let MONGODB_URI = process.env.MONGODB_URI
 const SECRET = process.env.SECRET
+const ROOT_PASSWORD = process.env.ROOT_PASSWORD
 
 if (process.env.NODE_ENV === 'test') {
   MONGODB_URI = process.env.MONGODB_TESTURI
@@ -12,4 +13,5 @@ module.exports = {
   MONGODB_URI,
   PORT,
   SECRET,
+  ROOT_PASSWORD,
 }
